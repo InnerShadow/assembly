d36349881eaec7d33ee6caeee748d9954911e7bc
sad ass <102435521+InnerShadow@users.noreply.github.com>
2023-02-10 23:31:51 +0300

    var 4, better variables

--- laba2_1.asm
+++ laba2_1.asm
@@ -6,10 +6,10 @@ ExitProcess proto, dwExitCode:dword
 
 .data
 
-_x1 SDWORD 2 ;5
-_x2 SDWORD 5 ;6
-_x3 SDWORD 8 ;4
-_x4 SDWORD 12 ;2
+_x1 SDWORD 6 
+_x2 SDWORD 5 
+_x3 SDWORD 2 
+_x4 SDWORD 12 
 
 it SDWORD 0
 
@@ -19,39 +19,39 @@ main proc
 	mov eax, it
 	BeginWile:
 		mov eax, it
-		cmp eax, 3
+		cmp eax, 5
 		jnl EndWhile
 
 		mov eax, _x1
 		cmp eax, _x2
-		jg Less1
+		jg More1
 		mov ebx, _x1
 		mov ecx, _x2
 		mov _x1, ecx
 		mov _x2, ebx
-		Less1:
+		More1:
 		jmp EndOfIf1
 		EndOfIf1:
 
 		mov eax, _x2
 		cmp eax, _x3
-		jg Less2
+		jg More2
 		mov ebx, _x2
 		mov ecx, _x3
 		mov _x2, ecx
 		mov _x3, ebx
-		Less2:
+		More2:
 		jmp EndOfIf2
 		EndOfIf2:
 
 		mov eax, _x3
 		cmp eax, _x4
-		jg Less3
+		jg More3
 		mov ebx, _x3
 		mov ecx, _x4
 		mov _x3, ecx
 		mov _x4, ebx
-		Less3:
+		More3:
 		jmp EndOfIf3
 		EndOfIf3:
 		inc it
