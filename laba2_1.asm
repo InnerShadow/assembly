0605fb9093e04cc546693744214cba1181e12c74
sad ass <102435521+InnerShadow@users.noreply.github.com>
2023-02-28 22:15:51 +0300

    Nazar, you pleather

--- laba2_1.asm
+++ laba2_1.asm
@@ -6,60 +6,68 @@ ExitProcess proto, dwExitCode:dword
 
 .data
 
-_x1 SDWORD 6 
-_x2 SDWORD 5 
-_x3 SDWORD 2 
-_x4 SDWORD 12 
+_x1 SDWORD 1 ; 6
+_x2 SDWORD 60 ; 5
+_x3 SDWORD 70 ; 12
+_x4 SDWORD 100 ; x1 > x2 > x3 > x4 ; 2
 
 it SDWORD 0
 
 .code
 main proc
 
-	mov eax, it
-	BeginWile:
-		mov eax, it
-		cmp eax, 5
-		jnl EndWhile
+	mov eax, _x1
+	cmp eax, _x2
+	jg More1
+		mov ebx, _x1
+		xchg ebx, _x2
+		mov _x1, ebx
+	More1:
 
-		mov eax, _x1
-		cmp eax, _x2
-		jg More1
-			mov ebx, _x1
-			mov ecx, _x2
-			mov _x1, ecx
-			mov _x2, ebx
-		More1:
+	mov eax, _x2
+	cmp eax, _x3
+	jg More2
+		mov ebx, _x2
+		xchg ebx, _x3
+		mov _x2, ebx
+	More2:
 
-		mov eax, _x2
-		cmp eax, _x3
-		jg More2
-			mov ebx, _x2
-			mov ecx, _x3
-			mov _x2, ecx
-			mov _x3, ebx
-		More2:
+	mov eax, _x3
+	cmp eax, _x4
+	jg More3
+		mov ebx, _x3
+		xchg ebx, _x4
+		mov _x3, ebx
+	More3:
+		jmp EndOfIf3
+	EndOfIf3:
+		
+	mov eax, _x1
+	cmp eax, _x2
+	jg More4
+		mov ebx, _x1
+		xchg ebx, _x2
+		mov _x1, ebx
+	More4:
 
-		mov eax, _x3
-		cmp eax, _x4
-		jg More3
-			mov ebx, _x3
-			mov ecx, _x4
-			mov _x3, ecx
-			mov _x4, ebx
-		More3:
-			jmp EndOfIf3
-		EndOfIf3:
-			inc it
-			jmp BeginWile
-	EndWhile:
-		mov it, eax
-
+	mov eax, _x2
+	cmp eax, _x3
+	jg More5
+		mov ebx, _x2
+		xchg ebx, _x3
+		mov _x2, ebx
+	More5:
 
 	mov eax, _x1
-	mov eax, _x2
-	mov eax, _x3
-	mov eax, _x4
+	cmp eax, _x2
+	jg More6
+		mov ebx, _x1
+		xchg ebx, _x2
+		mov _x1, ebx
+	More6:
+
+
+	
 
 	invoke ExitProcess,0
 
--- laba2_2.asm
+++ laba2_2.asm
@@ -6,102 +6,42 @@ ExitProcess proto, dwExitCode:dword
 
 .data
 
-k SDWORD 4
+k SDWORD 4 ; knight on cell (k;l) is posible to move on cell (m,n)?
 l SDWORD 4
 m SDWORD 5
-n SDWORD 2
+n SDWORD 3
 resualt BYTE 0
 
-result SWORD ?
 
 .code
 main proc
 
 	mov eax, k
-	add eax, 2
-	cmp eax, m
+	sub eax, m
+	imul eax
+	mov ebx, eax
+
+	mov eax, l
+	sub eax, n
+	imul eax
+
+	cmp ebx, 4
 	jne NOT1
-		mov eax, l
-		add eax, 1
-		cmp eax, n
+		cmp eax, 1
 		jne NOT2
-		mov resualt, 1
+			mov resualt, 1
 		NOT2:
 	NOT1:
 
-	mov eax, k
-	add eax, 1
-	cmp eax, m
+	cmp ebx, 1
 	jne NOT3
-		mov eax, l
-		add eax, 2
-		cmp eax, n
+		cmp eax, 4
 		jne NOT4
-		mov resualt, 1
+			mov resualt, 1
 		NOT4:
 	NOT3:
+	
 
-	mov eax, k
-	sub eax, 1
-	cmp eax, m
-	jne NOT5
-		mov eax, l
-		add eax, 2
-		cmp eax, n
-		jne NOT6
-		mov resualt, 1
-		NOT6:
-	NOT5:
-
-	mov eax, k
-	sub eax, 2
-	cmp eax, m
-	jne NOT7
-		mov eax, l
-		add eax, 1
-		cmp eax, n
-		jne NOT8
-		mov resualt, 1
-		NOT8:
-	NOT7:
-
-	mov eax, k
-	sub eax, 2
-	cmp eax, m
-	jne NOT9
-		mov eax, l
-		sub eax, 1
-		cmp eax, n
-		jne NOTA
-		mov resualt, 1
-		NOTA:
-	NOT9:
-
-	mov eax, k
-	sub eax, 1
-	cmp eax, m
-	jne NOTB
-		mov eax, l
-		sub eax, 2
-		cmp eax, n
-		jne NOTC
-		mov resualt, 1
-		NOTC:
-	NOTB:
-
-	mov eax, k
-	add eax, 1
-	cmp eax, m
-	jne NOTD
-		mov eax, l
-		sub eax, 2
-		cmp eax, n
-		jne NOTE
-		mov resualt, 1
-		NOTE:
-	NOTD:
-
-	mov al, resualt
 
 	invoke ExitProcess,0
 
